6061ccb0b0dec3052f862916fd149bfa3fe0a512
Mehluli Hikwa <mehlulihikwa@gmail.com>
2023-05-17 01:13:10 +0200

    Disable Zoom Feature

--- js/script.js
+++ js/script.js
@@ -1,24 +1,30 @@
+// 
 // Tooltip Options
 // https://atomiks.github.io/tippyjs/v6/customization/
+// 
 tippy('[data-tippy-content]', {
 	arrow: true,
 	delay: [100, 100],
 	followCursor: true,
 });
 
-svgPanZoom('.svg', {
-	viewportSelector: '.svg-pan-zoom_viewport',
-	panEnabled: true,
-	controlIconsEnabled: true,
-	zoomEnabled: true,
-	dblClickZoomEnabled: false,
-	mouseWheelZoomEnabled: true,
-	preventMouseEventsDefault: true,
-	zoomScaleSensitivity: 0.2,
-	minZoom: 0.5,
-	maxZoom: 10,
-	fit: true,
-	contain: false,
-	center: true,
-	refreshRate: 1
-});
+
+// 
+// Enable script below to experiment with Zoom feature
+// 
+// svgPanZoom('.svg', {
+// 	viewportSelector: '.svg-pan-zoom_viewport',
+// 	panEnabled: true,
+// 	controlIconsEnabled: true,
+// 	zoomEnabled: true,
+// 	dblClickZoomEnabled: false,
+// 	mouseWheelZoomEnabled: true,
+// 	preventMouseEventsDefault: true,
+// 	zoomScaleSensitivity: 0.2,
+// 	minZoom: 0.5,
+// 	maxZoom: 10,
+// 	fit: true,
+// 	contain: false,
+// 	center: true,
+// 	refreshRate: 1
+// });
