5c2e38d8d6b7de089c1612e654de661c011a322c
Mehluli Hikwa <mehlulihikwa@gmail.com>
2023-05-17 17:33:02 +0200

    Create Testing HTML for Modal

--- js/script.js
+++ js/script.js
@@ -1,36 +1,44 @@
 // 
-// Tooltip Options - https://atomiks.github.io/tippyjs/v6/customization/
+// TippyJS, Tooltip Options - https://atomiks.github.io/tippyjs/v6/customization/
 // 
 tippy('[data-tippy-content]', {
 	arrow: true,
 	delay: [100, 100],
-	followCursor: true,
+	followCursor: true
 });
 
 // 
-// Enable script below to experiment with Zoom feature
+// SVG Pan Zoom
+// https://github.com/ariutta/svg-pan-zoom
 //
-svgPanZoom('.svg', {
-	viewportSelector: '.svg-pan-zoom_viewport',
-	panEnabled: true,
-	controlIconsEnabled: true,
-	zoomEnabled: true,
-	dblClickZoomEnabled: false,
-	mouseWheelZoomEnabled: true,
-	preventMouseEventsDefault: true,
-	zoomScaleSensitivity: 0.2,
-	minZoom: 0.5,
-	maxZoom: 10,
-	fit: true,
-	contain: false,
-	center: true,
-	refreshRate: 'auto',
+// svgPanZoom('.svg', {
+// 	viewportSelector: '.svg-pan-zoom_viewport',
+// 	panEnabled: true,
+// 	controlIconsEnabled: true,
+// 	zoomEnabled: true,
+// 	dblClickZoomEnabled: false,
+// 	mouseWheelZoomEnabled: true,
+// 	preventMouseEventsDefault: true,
+// 	zoomScaleSensitivity: 0.2,
+// 	minZoom: 0.5,
+// 	maxZoom: 10,
+// 	fit: true,
+// 	contain: false,
+// 	center: true,
+// 	refreshRate: 'auto',
 
-	// beforeZoom: function(){},
-	// onZoom: function(){},
-	// beforePan: function(){},
-	// onPan: function(){},
-	// onUpdatedCTM: function(){},
-	// customEventsHandler: {},
-	// eventsListenerElement: null
-});
+// 	// beforeZoom: function(){},
+// 	// onZoom: function(){},
+// 	// beforePan: function(){},
+// 	// onPan: function(){
+// 	// 	console.log('I am Panning')
+// 	// },
+// 	// onUpdatedCTM: function(){
+// 	// 	document.getElementById("link").addEventListener('click', function(e){
+// 	// 		e.preventDefault()
+// 	// 		console.log('I was Clicked!', e);
+// 	// 	});
+// 	// },
+// 	// customEventsHandler: {},
+// 	// eventsListenerElement: null
+// });
